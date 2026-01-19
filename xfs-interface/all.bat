3887d84d1d9d593fcd6d2d0eaae4864014af3bec
Ebrahim525 <m.ebrahim.n123@gmail.com>
2026-01-19 17:43:19 +0530

    Stage 13

--- xfs-interface/all.bat
+++ xfs-interface/all.bat
@@ -2,5 +2,7 @@ load --os /home/expos/myexpos/spl/spl_progs/os_startup.xsm
 load --exhandler /home/expos/myexpos/spl/spl_progs/haltprog.xsm
 load --int=timer /home/expos/myexpos/spl/spl_progs/sample_timer.xsm
 load --int=7 /home/expos/myexpos/spl/spl_progs/sample_int7.xsm
-load --init /home/expos/myexpos/expl/expl_progs/squares.xsm
+load --int=10 /home/expos/myexpos/spl/spl_progs/haltprog.xsm
+load --init /home/expos/myexpos/expl/samples/numbers.xsm
 load --idle /home/expos/myexpos/expl/expl_progs/idle_code.xsm
+load --module 7 /home/expos/myexpos/spl/spl_progs/mod_7.xsm
---
+++ xfs-interface/mod.bat
@@ -0,0 +1,1 @@
+load --module 7 /home/expos/myexpos/spl/spl_progs/mod_7.xsm
