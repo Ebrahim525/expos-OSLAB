d9dfa3cf2163a43e3e9420e9dd0033b173faa2c3
Ebrahim525 <m.ebrahim.n123@gmail.com>
2026-02-05 11:08:49 +0530

    Stage 17

--- xfs-interface/all.bat
+++ xfs-interface/all.bat
@@ -4,8 +4,11 @@ load --int=timer /home/expos/myexpos/spl/spl_progs/timer.xsm
 load --int=console /home/expos/myexpos/spl/spl_progs/console.xsm
 load --int=6 /home/expos/myexpos/spl/spl_progs/int_6_read.xsm
 load --int=7 /home/expos/myexpos/spl/spl_progs/int_7.xsm
+load --int=9 /home/expos/myexpos/spl/spl_progs/int_9_exec.xsm
 load --int=10 /home/expos/myexpos/spl/spl_progs/int_10.xsm
 load --module 0 /home/expos/myexpos/spl/spl_progs/mod_0.xsm
+load --module 2 /home/expos/myexpos/spl/spl_progs/mod_1_process.xsm
+load --module 1 /home/expos/myexpos/spl/spl_progs/mod_2_memory.xsm
 load --module 4 /home/expos/myexpos/spl/spl_progs/mod_4.xsm
 load --module 7 /home/expos/myexpos/spl/spl_progs/mod_7.xsm
 load --module 5 /home/expos/myexpos/spl/spl_progs/mod_5.xsm
