f42088f99e930704a98f300c12bc53d418418ee9
Ebrahim525 <m.ebrahim.n123@gmail.com>
2026-02-05 15:19:39 +0530

    Stage 17 Bug Fix

--- xfs-interface/all.bat
+++ xfs-interface/all.bat
@@ -7,8 +7,8 @@ load --int=7 /home/expos/myexpos/spl/spl_progs/int_7.xsm
 load --int=9 /home/expos/myexpos/spl/spl_progs/int_9_exec.xsm
 load --int=10 /home/expos/myexpos/spl/spl_progs/int_10.xsm
 load --module 0 /home/expos/myexpos/spl/spl_progs/mod_0.xsm
-load --module 2 /home/expos/myexpos/spl/spl_progs/mod_1_process.xsm
-load --module 1 /home/expos/myexpos/spl/spl_progs/mod_2_memory.xsm
+load --module 1 /home/expos/myexpos/spl/spl_progs/mod_1_process.xsm
+load --module 2 /home/expos/myexpos/spl/spl_progs/mod_2_memory.xsm
 load --module 4 /home/expos/myexpos/spl/spl_progs/mod_4.xsm
 load --module 7 /home/expos/myexpos/spl/spl_progs/mod_7.xsm
 load --module 5 /home/expos/myexpos/spl/spl_progs/mod_5.xsm
