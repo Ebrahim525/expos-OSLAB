f378aa28840516dced3abe07f3451ed2e486a87a
Ebrahim525 <m.ebrahim.n123@gmail.com>
2026-03-16 02:16:35 +0530

    Stage22

--- xfs-interface/all.bat
+++ xfs-interface/all.bat
@@ -9,6 +9,8 @@ load --int=8 /home/expos/myexpos/spl/spl_progs/int_8_fork.xsm
 load --int=9 /home/expos/myexpos/spl/spl_progs/int_9_exec.xsm
 load --int=10 /home/expos/myexpos/spl/spl_progs/int_10_exit.xsm
 load --int=11 /home/expos/myexpos/spl/spl_progs/int_11_wait.xsm
+load --int=13 /home/expos/myexpos/spl/spl_progs/int_13_semget.xsm
+load --int=14 /home/expos/myexpos/spl/spl_progs/int_14_semlock.xsm
 load --int=15 /home/expos/myexpos/spl/spl_progs/int_15_shutdown.xsm
 load --module 0 /home/expos/myexpos/spl/spl_progs/mod_0_resource.xsm
 load --module 1 /home/expos/myexpos/spl/spl_progs/mod_1_process.xsm
