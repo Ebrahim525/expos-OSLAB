016af79a31c726c117bdcf63b10336c782234239
Ebrahim525 <m.ebrahim.n123@gmail.com>
2026-03-25 21:04:38 +0530

    Bug Fix

--- xfs-interface/all.bat
+++ xfs-interface/all.bat
@@ -4,6 +4,7 @@ load --int=timer /home/expos/myexpos/spl/spl_progs/timer.xsm
 load --int=disk /home/expos/myexpos/spl/spl_progs/disk.xsm
 load --int=console /home/expos/myexpos/spl/spl_progs/console.xsm
 load --int=4 /home/expos/myexpos/spl/spl_progs/int_4_file.xsm
+load --int=5 /home/expos/myexpos/spl/spl_progs/int_5_open.xsm
 load --int=6 /home/expos/myexpos/spl/spl_progs/int_6_read.xsm
 load --int=7 /home/expos/myexpos/spl/spl_progs/int_7_write.xsm
 load --int=8 /home/expos/myexpos/spl/spl_progs/int_8_fork.xsm
@@ -16,9 +17,10 @@ load --int=15 /home/expos/myexpos/spl/spl_progs/int_15_shutdown.xsm
 load --module 0 /home/expos/myexpos/spl/spl_progs/mod_0_resource.xsm
 load --module 1 /home/expos/myexpos/spl/spl_progs/mod_1_process.xsm
 load --module 2 /home/expos/myexpos/spl/spl_progs/mod_2_memory.xsm
+load --module 3 /home/expos/myexpos/spl/spl_progs/mod_3_file.xsm
 load --module 4 /home/expos/myexpos/spl/spl_progs/mod_4_device.xsm
-load --module 7 /home/expos/myexpos/spl/spl_progs/mod_7_boot.xsm
 load --module 5 /home/expos/myexpos/spl/spl_progs/mod_5_switch.xsm
+load --module 7 /home/expos/myexpos/spl/spl_progs/mod_7_boot.xsm
 load --init /home/expos/myexpos/expl/expl_progs/init.xsm
 load --idle /home/expos/myexpos/expl/expl_progs/idle_code.xsm
 load --library /home/expos/myexpos/expl/library.lib
